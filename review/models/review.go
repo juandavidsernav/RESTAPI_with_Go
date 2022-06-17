12e707b6fe540a392a6af2785f338a5981a437ba
Juan Serna <juandavidserna12@gmail.com>
2022-06-17 18:18:22 -0500

    reviews updated

--- review/models/review.go
+++ review/models/review.go
@@ -5,25 +5,30 @@ import (
 	"time"
 )
 
-const MaxLengthInComments = 400
+const maxLengthInComments = 400
 
+// Review represent an anon review from some website
 type Review struct {
-	Id      int64
-	Stars   int    //1 - 5
-	Comment string //max 400 chars
-	Date    time.Time
+	Id       int64
+	Stars    int       // 1 - 5
+	Comment  string    // max 400 chars
+	Date     time.Time // created at
+	GadgetId int64
 }
 
+// CreateReviewCMD command to create a new review
 type CreateReviewCMD struct {
 	Stars   int    `json:"stars"`
 	Comment string `json:"comment"`
+	GadgetId int64 `json:"gadget_id"`
 }
 
 func (cmd *CreateReviewCMD) validate() error {
 	if cmd.Stars < 1 || cmd.Stars > 5 {
-		return errors.New("starts must be between 1 to 5")
+		return errors.New("stars must be between 1 - 5")
 	}
-	if len(cmd.Comment) > MaxLengthInComments {
+
+	if len(cmd.Comment) > maxLengthInComments {
 		return errors.New("comment must be less than 400 chars")
 	}
 	return nil
